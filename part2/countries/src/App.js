e31c7b08471b2ba1e7961bcf3af7e634fbfbf724
sejalmishra <sejalmishra1310@gmail.com>
2022-11-21 20:14:56 +0530

    part 2 countries exercise completed

---
+++ part2/countries/src/App.js
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+//components
+import Countries from "./components/countries";
+
+function App() {
+  const [searchCountry, setSearchCountry] = useState("");
+  const [countries, setCountries] = useState([]);
+  const [weatherData, setWeatherData] = useState({});
+
+  function fetchData() {
+    const name = searchCountry;
+    if (name !== "") {
+      const res = axios.get(`https://restcountries.com/v3.1/name/${name}`);
+      res.then((res) => {
+        const filteredCountries = res.data.map((r) => {
+          return r;
+        });
+        setCountries(filteredCountries);
+        if (filteredCountries.length === 1) {
+          getWeatherData(countries[0].name.common);
+        }
+      });
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [searchCountry]);
+
+  const getWeatherData = (cityName) => {
+    const res = axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+    );
+    res.then((res) => {
+      setWeatherData(res.data);
+    });
+  };
+
+  return (
+    <div>
+      Find Countries
+      <input
+        type="text"
+        value={searchCountry}
+        onChange={(event) => setSearchCountry(event.target.value)}
+      />
+      {searchCountry !== "" ? (
+        <Countries countries={countries} weatherData={weatherData} />
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ part2/countries/src/components/countries.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+function Countries({ countries, weatherData }) {
+  const [isCountryDetail, setCountryDetail] = useState(false);
+  const [viewCountry, setViewCountry] = useState({});
+
+  const manageView = (data) => {
+    setCountryDetail(true);
+    setViewCountry(data);
+  };
+
+  if (countries.length > 10) {
+    return <p>Too many matches, specify another filter</p>;
+  }
+
+  if (countries.length === 1) {
+    return (
+      <div>
+        <h1>{countries[0].name.common}</h1>
+        <p>capital {countries[0].capital[0]}</p>
+        <p>area {countries[0].area}</p>
+        <h2>Languages:</h2>
+        {Object.values(countries[0].languages).map((value, i) => (
+          <li key={i}>
+            <span>{value}</span>
+          </li>
+        ))}
+        <img src={countries[0].flags.png} />
+
+        <h2>Weather in {countries[0].capital[0]}</h2>
+        <p>temprature - {weatherData?.main?.temp} Celcius</p>
+        <img
+          src={`http://openweathermap.org/img/wn/${weatherData?.weather[0]?.icon}@2x.png`}
+        />
+        <p>wind - {weatherData?.wind?.speed} m/s</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {countries.map((country) => {
+        return (
+          <p key={country.name.common}>
+            <b>{country.name.common}</b> with official name{" "}
+            {country.name.official}
+            <button onClick={() => manageView(country)}>show</button>
+          </p>
+        );
+      })}
+      {isCountryDetail && (
+        <div>
+          <h1>{viewCountry.name.common}</h1>
+          <p>capital {viewCountry.capital[0]}</p>
+          <p>area {viewCountry.area}</p>
+          <h2>Languages:</h2>
+          {Object.values(viewCountry.languages).map((value, i) => (
+            <li key={i}>
+              <span>{value}</span>
+            </li>
+          ))}
+          <img src={viewCountry.flags.png} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Countries;
